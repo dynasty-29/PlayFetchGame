c3b50b8870a00dcf2a53daa1d1959cbe2a49524d
Sonnie <mgathoni.gathoni9@gmail.com>
2023-09-23 17:43:36 +0300

    Create PlayerControllerX.cs

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogDelay = 1f;
+    public float lastDog = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Time.time - lastDog>= dogDelay)
+            {
+                GameObject dog = Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                dog.transform.parent = transform;
+
+                lastDog = Time.time;
+            }
+        }
+    }
+}
